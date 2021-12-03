50a6a126417859e3cc3c90c745d9d09bd250e48d
AdemKao <adem60513@gmail.com>
2021-12-03 08:42:32 +0800

    add index.js

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default () => (
+  <>
+    <div>About the NEXT Web</div>
+    <div>
+      <Link href="/">
+        <a>back to Index</a>
+      </Link>
+    </div>
+  </>
+);
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default () => (
+  <>
+    <div>My First NEXT WEB</div>
+    <div>
+      <Link href="/about">
+        <a>About Next </a>
+      </Link>
+    </div>
+  </>
+);
